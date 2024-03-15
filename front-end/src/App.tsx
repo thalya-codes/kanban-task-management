07ea896128492db1590af1801b65fe5396baa940
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-15 19:26:30 -0300

    refactor(App): remove componente modal

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,41 +1,9 @@
-import { Modal } from './components/library/Modal/dialog';
+import { Modal } from './components/library/Modal';
 
 function App() {
   return (
     <>
       <h1>Kanban Task Management</h1>
-      return (
-      <Modal.Dialog>
-        <Modal.Trigger asChild>
-          <button>Edit Profile</button>
-        </Modal.Trigger>
-        <Modal.Content className='sm:max-w-[425px]'>
-          <Modal.Header>
-            <Modal.Title>Edit profile</Modal.Title>
-            <Modal.Description>
-              Make changes to your profile here. Click save when you're done.
-            </Modal.Description>
-          </Modal.Header>
-          <div className='grid gap-4 py-4'>
-            <div className='grid grid-cols-4 items-center gap-4'>
-              <label htmlFor='name' className='text-right'>
-                Name
-              </label>
-              <input id='name' value='Pedro Duarte' className='col-span-3' />
-            </div>
-            <div className='grid grid-cols-4 items-center gap-4'>
-              <label htmlFor='username' className='text-right'>
-                Username
-              </label>
-              <input id='username' value='@peduarte' className='col-span-3' />
-            </div>
-          </div>
-          <Modal.Footer>
-            <button type='submit'>Save changes</button>
-          </Modal.Footer>
-        </Modal.Content>
-      </Modal.Dialog>
-      )
     </>
   );
 }
