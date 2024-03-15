2e62a0dc2a0612976f6918acf5a11a9cc43d6d0b
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-15 19:44:36 -0300

    refactor(Dropdown): cria arquivo para cada composição  - Exclui componentes que não serão utilizados

---
+++ front-end/src/components/library/Dropdown/index.tsx
@@ -0,0 +1,10 @@
+import { Root, Trigger } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuContent } from './DropdownContent';
+import { DropdownMenuItem } from './DropdownMenuItem';
+
+export const Dropdown = {
+  Root,
+  Trigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+};
