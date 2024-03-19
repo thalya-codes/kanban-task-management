a4c1c9ad8beddcb32151b0742868961f4289bf4f
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-19 08:15:18 -0300

    feat(components/project/Button): cria interface

---
+++ front-end/src/components/project/Button/interface.ts
@@ -0,0 +1,7 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  variant?: 'purple' | 'light-purple' | 'red' | 'link-medium-gray' | 'link-red';
+  children: ReactNode;
+  hover?: string;
+}
