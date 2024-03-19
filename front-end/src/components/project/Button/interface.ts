e8f782a8b8f65168d4185c570b248ecf07ebd112
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-19 08:20:56 -0300

    feat(components/project/Button): remove prop não utilizada na interface IButtonProps

--- front-end/src/components/project/Button/interface.ts
+++ front-end/src/components/project/Button/interface.ts
@@ -3,5 +3,4 @@ import { HTMLAttributes, ReactNode } from 'react';
 export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
   variant?: 'purple' | 'light-purple' | 'red' | 'link-medium-gray' | 'link-red';
   children: ReactNode;
-  hover?: string;
 }
