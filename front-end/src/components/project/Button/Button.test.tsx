3ccc3f0b880d56317d027ebba6164328bddb5e26
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-21 17:09:04 -0300

    feat(project/components/Button): cria teste que verifica a chamada da função passada

--- front-end/src/components/project/Button/Button.test.tsx
+++ front-end/src/components/project/Button/Button.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { Button } from '.';
 
 describe('Should verify if the Button component is working correctly', () => {
@@ -8,5 +9,16 @@ describe('Should verify if the Button component is working correctly', () => {
 
     const button = screen.getByText('Add new');
     expect(button).toBeInTheDocument();
+  });
+
+  it('should call the function at the click in the button', async () => {
+    const handleClick = jest.fn();
+
+    render(<Button onClick={handleClick}>Button</Button>);
+
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
