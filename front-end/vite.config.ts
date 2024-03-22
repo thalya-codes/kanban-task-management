d320cc17568433676822e48ec3cf52a468191307
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-22 16:45:27 -0300

    chore(vit.config): adiciona plugin svgr

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
       '@': path.resolve('src'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
