cc5e329d63d974df15f53cee38e74a18669ccb5b
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-22 22:37:00 -0300

    feat(components/project): cria componente ThemeSwitcher

---
+++ front-end/src/components/project/ThemeSwitcher/index.tsx
@@ -0,0 +1,15 @@
+import { Switch } from '@/components/library/Switch';
+import DarkThemeIcon from '@/assets/icon-dark-theme.svg?react';
+import LightThemeIcon from '@/assets/icon-light-theme.svg?react';
+import { cn } from '@/utils/cn';
+import { ThemeSwitcherVariants } from './variants';
+
+export function ThemeSwitcher({ mode = 'light' }: { mode: 'light' | 'dark' }) {
+  return (
+    <div className={cn(ThemeSwitcherVariants({ mode }))}>
+      <LightThemeIcon />
+      <Switch />
+      <DarkThemeIcon />
+    </div>
+  );
+}
