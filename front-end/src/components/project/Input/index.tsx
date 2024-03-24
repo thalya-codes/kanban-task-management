d04baac8d4566f0c6f03baf5796f95a4dba6ccb9
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-23 23:53:33 -0300

    feat(project/Input): exporta composições

---
+++ front-end/src/components/project/Input/index.tsx
@@ -0,0 +1,9 @@
+import { InputRoot } from './InputRoot';
+import { InputErrorMsg } from './InputErrorMsg';
+import { InputField } from '@/components/library/InputField';
+
+export const Input = {
+  Root: InputRoot,
+  Field: InputField,
+  ErrorMsg: InputErrorMsg,
+};
