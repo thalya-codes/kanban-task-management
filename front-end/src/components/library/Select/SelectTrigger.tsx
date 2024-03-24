828754b0f85a114baa42d054622e85ff4f6b149c
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-23 23:58:52 -0300

    feat(library/SelectTrigger): customiza estilização

--- front-end/src/components/library/Select/SelectTrigger.tsx
+++ front-end/src/components/library/Select/SelectTrigger.tsx
@@ -10,14 +10,14 @@ export const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'aria-expanded:border-primary-purple outline-none flex h-10 w-full items-center justify-between rounded-sm border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       className
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className='h-4 w-4 opacity-50' />
+      <ChevronDown className='h-4 w-4 font-bold text-primary-purple' />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
