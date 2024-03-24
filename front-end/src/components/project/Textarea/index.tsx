0068274a52f8061f17a04d8552854ef3b3a7483d
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-24 00:14:54 -0300

    feat(project/Textarea): cria componente

---
+++ front-end/src/components/project/Textarea/index.tsx
@@ -0,0 +1,17 @@
+import { TextareaHTMLAttributes } from 'react';
+import { cn } from '@/utils/cn';
+
+export function Textarea({
+  className,
+  ...props
+}: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return (
+    <textarea
+      className={cn(
+        'border rounded-sm outline-none focus:border-primary-purple px-3 py-2',
+        className
+      )}
+      {...props}
+    />
+  );
+}
