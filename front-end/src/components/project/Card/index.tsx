01a336c7f8a448a0c9606e301255e382f9e7a614
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-24 22:45:16 -0300

    feat(components/project): cria  componente Card

---
+++ front-end/src/components/project/Card/index.tsx
@@ -0,0 +1,24 @@
+import { Typography } from '@/components/project/Typography';
+import { ICardProps } from './interface';
+
+export function Card({
+  title,
+  totalSubTasks,
+  completedSubTasks,
+  onClick,
+}: ICardProps) {
+  return (
+    <button
+      className='flex flex-col gap-1 rounded-md [&>h3]:hover:text-primary-purple [&>*]:text-start shadow-elevation-1 p-5 w-[280px]'
+      onClick={onClick}
+    >
+      <Typography as='h3' color='black'>
+        {title}
+      </Typography>
+
+      <Typography as='h4' size='xs'>
+        {completedSubTasks} of {totalSubTasks} subtasks
+      </Typography>
+    </button>
+  );
+}
