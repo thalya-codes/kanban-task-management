071e3377d36ebedf57a9bc0c450e9e5f7d642d07
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-26 21:46:59 -0300

    feat(project/Button): adiciona nova variant

--- front-end/src/components/project/Button/interface.ts
+++ front-end/src/components/project/Button/interface.ts
@@ -1,6 +1,12 @@
 import { HTMLAttributes, ReactNode } from 'react';
 
 export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  variant?: 'purple' | 'light-purple' | 'red' | 'link-medium-gray' | 'link-red';
+  variant?:
+    | 'purple'
+    | 'light-purple'
+    | 'red'
+    | 'link-medium-gray'
+    | 'link-red'
+    | 'drawer';
   children: ReactNode;
 }
--- front-end/src/components/project/Button/variants.ts
+++ front-end/src/components/project/Button/variants.ts
@@ -11,6 +11,8 @@ export const ButtonVariants = cva(
         red: 'bg-primary-red hover:bg-primary-light-red text-white',
         'link-medium-gray': 'text-neutral-medium-gray hover:underline',
         'link-red': 'text-primary-red hover:underline',
+        drawer:
+          '[&>svg]:ml-10 text-md h-12 rounded-3xl flex justify-center items-center gap-3 text-neutral-medium-gray hover:bg-primary-extra-light-purple data-[drawer-active=true]:bg-primary-purple data-[drawer-active=true]:hover:bg-primary-purple data-[drawer-active=true]:text-white data-[drawer-active=false]:[&_span]:hover:text-primary-purple',
       },
     },
   }
