34af2c6a7e2e0bd5398c43c0a6448f7cab0feced
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-26 21:59:00 -0300

    feat(project/Drawer): cria interface para o componente DrawerContent

---
+++ front-end/src/components/project/Drawer/interface.ts
@@ -0,0 +1,9 @@
+import { Dispatch, SetStateAction } from 'react';
+
+export interface IDrawerContentProps {
+  show: boolean;
+  onShow: Dispatch<SetStateAction<boolean>>;
+  boards: { id: string; name: string; onClick: (id: string) => void }[];
+  activeBoardId: string;
+  onCreateNewBoard: () => void;
+}
