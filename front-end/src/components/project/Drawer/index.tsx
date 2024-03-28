5c3c25326a7cab90d10b6befb6defe6e07bf8066
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-03-28 15:03:48 -0300

    feat(project/Drawer): exporta composições

---
+++ front-end/src/components/project/Drawer/index.tsx
@@ -0,0 +1,9 @@
+import { DrawerContent } from './DrawerContent';
+import { DrawerRoot } from './DrawerRoot';
+import { DrawerTrigger } from './DrawerTrigger';
+
+export const Drawer = {
+  Root: DrawerRoot,
+  Trigger: DrawerTrigger,
+  Content: DrawerContent,
+};
