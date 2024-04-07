3299899b8fbb08db0ff1a63a6b30b415344d57bf
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-07 20:34:23 -0300

    feat(project/Column): exporta composições

---
+++ front-end/src/components/project/Column/index.tsx
@@ -0,0 +1,9 @@
+import { ColumnArea } from "./ColumnArea";
+import { ColumnItems } from "./ColumnItems";
+import { ColumnRoot } from "./ColumnRoot";
+
+export const Column = {
+    Root: ColumnRoot,
+    Area: ColumnArea,
+    Items: ColumnItems,
+}
