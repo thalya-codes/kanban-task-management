1c38509882784a7b063d1f310103fd271059b3ca
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-09 16:37:58 -0300

    feat(project/ColumnArea): implementa 'header'

--- front-end/src/components/project/Column/ColumnArea.tsx
+++ front-end/src/components/project/Column/ColumnArea.tsx
@@ -2,13 +2,23 @@ import { Droppable } from 'react-beautiful-dnd';
 import { IColumnAreaProps } from './interfaces';
 import { Typography } from '@/components/project/Typography';
 import { Column } from '.';
+import { ColorPickerModal } from '../ColorPickerModal';
 
-export function ColumnArea({ id, title, index, tasks }: IColumnAreaProps) {
+export function ColumnArea({
+  id,
+  title,
+  index,
+  tasks,
+  color,
+}: IColumnAreaProps) {
   return (
     <Droppable droppableId={id} key={index}>
       {(provided) => (
         <div>
-          <Typography as='h3'>{title.toUpperCase()}</Typography>
+          <div className='flex items-center gap-2'>
+            <ColorPickerModal />
+            <Typography as='h3'>{title.toUpperCase()}</Typography>
+          </div>
 
           <ul
             className={`${id} flex flex-col gap-10 h-screen`}
