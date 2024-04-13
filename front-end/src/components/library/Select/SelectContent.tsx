9db79a5d40cf99b6c7678e783e8f2b3609fdb349
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-13 16:58:03 -0300

    fix(SelectContent): ajusta bg

--- front-end/src/components/library/Select/SelectContent.tsx
+++ front-end/src/components/library/Select/SelectContent.tsx
@@ -11,7 +11,7 @@ export const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        'relative left-[2px] z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-sm border border-primary-purple bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'relative left-[2px] z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-sm border border-primary-purple bg-white text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         position === 'popper' &&
           'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-0 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
         className
