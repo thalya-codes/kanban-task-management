a588fdaa08a6e90e04c82a3f11535bf882f1f07b
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-13 16:50:42 -0300

    fix(ButtonVariants): ajusta font-weight e text size

--- front-end/src/components/project/Button/variants.ts
+++ front-end/src/components/project/Button/variants.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const ButtonVariants = cva(
-  'rounded-2xl py-2 px-5 transition-all w-full font-medium text-[10px]',
+  'rounded-2xl py-2 px-5 transition-all w-full font-medium text-sm',
   {
     variants: {
       variant: {
@@ -9,8 +9,9 @@ export const ButtonVariants = cva(
         'light-purple':
           'bg-primary-extra-light-purple hover:bg-primary-medium-purple text-primary-purple',
         red: 'bg-primary-red hover:bg-primary-light-red text-white',
-        'link-medium-gray': 'text-neutral-medium-gray hover:underline',
-        'link-red': 'text-primary-red hover:underline',
+        'link-medium-gray':
+          'text-neutral-medium-gray hover:underline font-normal',
+        'link-red': 'text-primary-red hover:underline font-normal',
         drawer:
           '[&>svg]:ml-10 text-md h-12 rounded-3xl flex justify-center items-center gap-3 text-neutral-medium-gray hover:bg-primary-extra-light-purple data-[drawer-active=true]:bg-primary-purple data-[drawer-active=true]:hover:bg-primary-purple data-[drawer-active=true]:text-white data-[drawer-active=false]:[&_span]:hover:text-primary-purple',
       },
