a8730d05b9a8d303a664e2fac193e2de411d2e45
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-23 18:49:06 -0300

    feat(back-end/routes): cria funções do endpoint register

---
+++ back-end/routes/register/functions.ts
@@ -0,0 +1,35 @@
+import { hash } from 'bcrypt';
+import { Response } from 'express';
+import { UserModel } from '@/database/Models/UserModel';
+import { HttpError } from '@/errors/HttpError';
+import { IUser } from '@/interfaces';
+
+export async function treatRegisterUserErrors({
+  email,
+  name,
+  password,
+}: IUser) {
+  if (!email || !name || !password)
+    throw new HttpError(
+      "Os campos: 'email', 'password' e 'name' são obrigatórios. Por favor, verifique se todos foram inseridos.",
+      400
+    );
+
+  const user = await UserModel.findOne({ email });
+
+  if (user?.email) {
+    throw new HttpError(
+      'Já existe um usuário cadastrado com esse e-mail. Por favor, escolha outro e-mail válido ou realize login.',
+      409
+    );
+  }
+}
+
+export async function registerUser(
+  response: Response,
+  { email, password, ...restBody }: IUser
+) {
+  const passwordHash = await hash(password, 8);
+  await UserModel.create({ email, password: passwordHash, ...restBody });
+  response.status(201).json({ message: 'Usuário criado com sucesso.' });
+}
