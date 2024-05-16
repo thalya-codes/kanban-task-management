45ca262d4f59112dbb5a9b613e74e2e7e2326940
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-16 10:10:34 -0300

    feat(back-end): cria middleware verifyAuthorization

---
+++ back-end/src/utils/auth/verifyAuthorization.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from './';
+import { NOT_AUTHORIZATED } from '../../errors';
+import { RevokedTokensModel } from '../../models';
+
+export const verifyAuthorization = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const token = request.headers.authorization;
+
+  if (!token) {
+    return response.status(401).json({ message: NOT_AUTHORIZATED });
+  }
+
+  try {
+    const decodedInfos = verifyToken({ token, response });
+
+    if (!decodedInfos) return;
+
+    const {
+      payload: { _id },
+    } = decodedInfos as any;
+
+    const user = await RevokedTokensModel.findOne({ userId: _id });
+    if (!user) return;
+
+    const informedTokenWasRevoked = user.tokens.some(
+      ({ value }) => value === token
+    );
+
+    if (informedTokenWasRevoked) {
+      return response.status(401).json({ message: NOT_AUTHORIZATED });
+    }
+
+    next();
+  } catch (error) {
+    response.status(401).json({ message: NOT_AUTHORIZATED });
+  }
+};
