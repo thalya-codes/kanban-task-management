5e83ce739f7a78eb72c767298abebe4f109bd102
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-16 10:16:27 -0300

    feat(back-end): cria UserController

---
+++ back-end/src/controllers/UserController.ts
@@ -0,0 +1,36 @@
+import { Response } from 'express';
+import { generateJWT, validateCredentials } from '../utils/auth';
+import { validateFields, createUser } from '../utils';
+import { IUserRequest } from '../interfaces';
+
+export class UserController {
+  static async register(
+    { body }: IUserRequest<{ name: string }>,
+    response: Response
+  ) {
+    const { name, email, password } = body;
+
+    try {
+      validateFields({ name, email, password });
+      await createUser(response, body);
+    } catch (error: any) {
+      response
+        .status(error.status || 500)
+        .json({ message: error.message || error });
+    }
+  }
+
+  static async login(
+    { body: { email, password } }: IUserRequest,
+    response: Response
+  ) {
+    try {
+      const user = await validateCredentials({ email, password });
+      await generateJWT({ _id: user._id! }, response);
+    } catch (error: any) {
+      response
+        .status(error.status || 500)
+        .json({ message: error?.message || error });
+    }
+  }
+}
