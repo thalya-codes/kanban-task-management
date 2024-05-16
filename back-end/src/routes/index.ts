7c6b54d1f1e10d3fb7a4b1cbb4d4124f2cbd479d
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-16 10:19:40 -0300

    feat(back-end): cria rotas

---
+++ back-end/src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { verifyAuthorization } from '../utils/auth';
+import {
+  UserController,
+  BoardsController,
+  RevokedTokensController,
+} from '../controllers';
+
+const router = Router();
+
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/logout', RevokedTokensController.addToRevokedList);
+router.get('/boards/:userId', verifyAuthorization, BoardsController.getAll);
+
+export default router;
