be116716999738bae67c3629ad3c75c684770d15
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-17 15:40:52 -0300

    refactor(front-end/pages): substitui parte do conteúdo pelo FormBase

--- front-end/src/pages/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage.tsx
@@ -1,42 +1,11 @@
-import OpenEye from '@/assets/eye.svg?react';
-import HideEye from '@/assets/icon-hide.svg?react';
-import { Button, FieldContainer, Input } from '@/components/project';
-import { useTogglePasswordType } from '@/hooks/useTogglePasswordType';
+import { FieldContainer, Input, FormBase } from '@/components/project';
 
 export function RegisterPage() {
-  const { passwordType, togglePasswordType } = useTogglePasswordType();
-
   return (
-    <form className='flex flex-col gap-10 w-3/6 sm:w-5/6'>
-      <FieldContainer htmlFor='name' color='black' text='Name'>
-        <Input.Field placeholder='My name' id='name' />
+    <FormBase isLoginForm={false} onClick={() => {}}>
+      <FieldContainer htmlFor='name' color='purple' text='Name'>
+        <Input.Field placeholder='Your name' id='name' />
       </FieldContainer>
-
-      <FieldContainer htmlFor='email' color='black' text='Email'>
-        <Input.Field id='email' type='email' placeholder='email@email.com' />
-      </FieldContainer>
-
-      <FieldContainer htmlFor='password' color='black' text='Email'>
-        <Input.Root>
-          <Input.Field id='password' type={passwordType} />
-
-          <button
-            type='button'
-            onClick={togglePasswordType}
-            aria-label={
-              passwordType === 'password' ? 'Show password' : 'Hide password'
-            }
-          >
-            {passwordType === 'password' ? (
-              <OpenEye className='w-8 h-8 bg-white' />
-            ) : (
-              <HideEye className='w-8 h-8 bg-white' />
-            )}
-          </button>
-        </Input.Root>
-      </FieldContainer>
-
-      <Button>Register</Button>
-    </form>
+    </FormBase>
   );
 }
