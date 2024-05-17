3031b84da15a459210b4f405e6f3a33252552417
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-17 15:44:43 -0300

    feat(front-end/routes): cria rota de login

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { PublicLayout } from '@/layouts/PublicLayout';
-import { RegisterPage } from '@/pages/RegisterPage';
+import { RegisterPage, LoginPage } from '@/pages';
 
 export function Router() {
   return (
     <Routes>
       <Route element={<PublicLayout />}>
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Route>
     </Routes>
   );
