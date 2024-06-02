e824f1eba2d97f8cd6bb09e4f4ee1534bc442274
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-02 17:04:06 -0300

    feat(front-end): adiciona QueryClientProvider

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import '@/globals.css';
 import { Router } from './routes';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryCliente = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryCliente}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
