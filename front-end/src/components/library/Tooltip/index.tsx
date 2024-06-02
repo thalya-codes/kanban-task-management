a268040b7c32e8c292e70bcc9dd25621e7c10efc
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-02 16:11:41 -0300

    feat(front-end): exporta composições do Tooltip

---
+++ front-end/src/components/library/Tooltip/index.tsx
@@ -0,0 +1,9 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { TooltipContent } from './TooltipContent';
+
+export const Tooltip = {
+  Root: TooltipPrimitive.Root,
+  Provider: TooltipPrimitive.Provider,
+  Trigger: TooltipPrimitive.Trigger,
+  Content: TooltipContent,
+};
