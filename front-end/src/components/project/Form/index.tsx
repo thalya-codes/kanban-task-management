cfe47a2bcfac01b6bdd6bbf8f2261239ed262a8c
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-02 17:01:05 -0300

    feat(front-end): exporta FormFooter

--- front-end/src/components/project/Form/index.tsx
+++ front-end/src/components/project/Form/index.tsx
@@ -1,7 +1,9 @@
 import { FormRoot } from './FormRoot';
 import { FormPasswordField } from './FormPasswordField';
+import { FormFooter } from './FormFooter';
 
 export const Form = {
   Root: FormRoot,
   PasswordField: FormPasswordField,
+  Footer: FormFooter,
 };
