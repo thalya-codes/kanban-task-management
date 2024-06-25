8a99fef9a1a43b3da3556488371bedec0b8a5113
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-25 14:28:30 -0300

    feat(BoardsController): cria método createBoard

--- back-end/src/controllers/BoardController.ts
+++ back-end/src/controllers/BoardController.ts
@@ -1,19 +1,48 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { UserModel } from '../models/UserModel';
-import { INVALID_USER_ID } from '../errors';
+import {
+  HttpError,
+  INVALID_USER_ID,
+  NOT_FOUNDED,
+  SUCCESSFUL_OPERATION,
+} from '../errors';
 import { IBoardRequest } from '../interfaces';
 
 export class BoardsController {
- static async getAll({ params }: IBoardRequest, response: Response) {
-  try {
-   const user = await UserModel.findOne({ _id: params.userId });
+  static async createBoard(
+    { params: { userId }, body }: Request,
+    response: Response
+  ) {
+    try {
+      const user = await UserModel.findByIdAndUpdate(
+        userId,
+        { $push: { boards: body } },
+        { new: true, runValidators: true }
+      );
 
-   if (!user) return response.status(400).json({ message: INVALID_USER_ID });
-   response.status(200).json({ boards: user?.boards || [] });
-  } catch (error: any) {
-   response
-    .status(error.status || 500)
-    .json({ message: error?.message || error });
+      if (!user) {
+        throw new HttpError(NOT_FOUNDED, 404);
+      }
+
+      response.status(200).json({
+        message: SUCCESSFUL_OPERATION,
+        status: 200,
+        data: user.boards,
+      });
+    } catch (error: any) {
+      response.status(error.code).json(error);
+    }
   }
- }
+  static async getAll({ params }: IBoardRequest, response: Response) {
+    try {
+      const user = await UserModel.findOne({ _id: params.userId });
+
+      if (!user) return response.status(400).json({ message: INVALID_USER_ID });
+      response.status(200).json({ boards: user?.boards || [] });
+    } catch (error: any) {
+      response
+        .status(error.status || 500)
+        .json({ message: error?.message || error });
+    }
+  }
 }
