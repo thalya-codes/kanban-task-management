997cefe5216fc7e6a28fcbbed34ed43d868eb415
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-25 15:13:40 -0300

    feat(generateJWTs): lança erro

--- back-end/src/utils/auth/generateJWT.ts
+++ back-end/src/utils/auth/generateJWT.ts
@@ -1,20 +1,28 @@
 import JWT from 'jsonwebtoken';
 import { Response } from 'express';
 import { IGenerateJWTPayload } from '../../interfaces';
-import { FAIL_TOKEN_GENERATION, SUCCESSFUL_OPERATION } from '../../errors';
+import {
+  FAIL_TOKEN_GENERATION,
+  HttpError,
+  SUCCESSFUL_OPERATION,
+} from '../../errors';
 
 export async function generateJWT(
- payload: IGenerateJWTPayload,
- response: Response
+  payload: IGenerateJWTPayload,
+  response: Response
 ) {
- JWT.sign(
-  { payload },
-  process.env.JWT_SECRET as string,
-  { expiresIn: '5d' , algorithm: 'HS384'},
-  (err: Error | null, token: string | undefined) => {
-   if (err) response.status(500).json({ message: FAIL_TOKEN_GENERATION });
+  JWT.sign(
+    { payload },
+    process.env.JWT_SECRET as string,
+    { expiresIn: '5d', algorithm: 'HS384' },
+    (err: Error | null, token: string | undefined) => {
+      if (err) {
+        throw new HttpError(FAIL_TOKEN_GENERATION, 500);
+      }
 
-   response.status(200).json({ token, message: SUCCESSFUL_OPERATION });
-  }
- );
+      return response
+        .status(200)
+        .json({ token, message: SUCCESSFUL_OPERATION });
+    }
+  );
 }
